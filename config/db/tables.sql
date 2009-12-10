a92d3e8f1edc91b011b515b1033a039a3035b568
kstenschke <kstenschke@7f067039-fbb2-4aac-a504-d491c780d6a3>
2009-12-10 12:01:31 +0000

    * began implementing DB comparism into installer (done: finding missing tables) * added first few extensions' 'tables.sql' definitions * split installer methods into sub classes

---
+++ config/db/tables.sql
@@ -0,0 +1,13 @@
+--
+-- Table structure for table `ext_bookmark_bookmark`
+--
+
+CREATE TABLE `ext_bookmark_bookmark` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user_create` int(10) unsigned NOT NULL,
+  `date_create` int(10) unsigned NOT NULL DEFAULT '0',
+  `type` tinyint(1) NOT NULL,
+  `deleted` tinyint(1) NOT NULL,
+  `id_item` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
