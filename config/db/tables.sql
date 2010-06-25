bacde1d2f558b01054fe50d213cbbd2e9599ac3f
kstenschke <kstenschke@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-06-25 09:38:13 +0000

    * adjusted all SQL files to comply with coding convention

--- config/db/tables.sql
+++ config/db/tables.sql
@@ -3,12 +3,12 @@
 --
 
 CREATE TABLE `ext_bookmark_bookmark` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `date_create` int(10) unsigned NOT NULL DEFAULT '0',
-  `date_update` int(10) unsigned NOT NULL DEFAULT '0',
-  `id_person_create` int(10) unsigned NOT NULL,
-  `deleted` tinyint(1) NOT NULL,
-  `type` tinyint(1) NOT NULL,
-  `id_item` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id`)
+	`id` int(10) NOT NULL AUTO_INCREMENT,
+	`date_create` int(10) unsigned NOT NULL DEFAULT '0',
+	`date_update` int(10) unsigned NOT NULL DEFAULT '0',
+	`id_person_create` int(10) unsigned NOT NULL,
+	`deleted` tinyint(1) NOT NULL,
+	`type` tinyint(1) NOT NULL,
+	`id_item` int(10) unsigned NOT NULL,
+	PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
