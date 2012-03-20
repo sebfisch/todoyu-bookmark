a4078ecd36307ee48f1856e66a0477b63ff523d5
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2012-03-20 09:27:58 +0000

    * Bookmark version cleanup

--- config/extinfo.php
+++ config/extinfo.php
@@ -31,7 +31,7 @@ Todoyu::$CONFIG['EXT']['bookmark']['info']	= array(
 		'company'	=> 'snowflake productions GmbH, Zurich'
 	),
 	'state'			=> 'stable',
-	'version'		=> '1.1.2',
+	'version'		=> '1.2.0',
 	'constraints'	=> array(
 		'depends'	=> array(
 			'contact'	=> '1.1.0',
