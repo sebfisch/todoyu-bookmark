d6f250d5ef5d583448138fa905acaa0657519480
nkarrer <nkarrer@7f067039-fbb2-4aac-a504-d491c780d6a3>
2013-03-27 10:25:10 +0000

    * Version fix

--- config/extinfo.php
+++ config/extinfo.php
@@ -31,7 +31,7 @@ Todoyu::$CONFIG['EXT']['bookmark']['info']	= array(
 		'company'	=> 'snowflake productions GmbH, Zurich'
 	),
 	'state'			=> 'stable',
-	'version'		=> '1.2.1',
+	'version'		=> '1.2.2',
 	'constraints'	=> array(
 		'core'		=> '2.3.0',
 		'depends'	=> array(
